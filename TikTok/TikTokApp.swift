af4505516f19cfc1aa9a77b80449a3ff3454339d
Todd H. Albert <todd@bocacode.com>
2024-05-26 13:36:53 -0400

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Todd Albert on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
